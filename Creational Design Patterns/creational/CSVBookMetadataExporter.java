b017c3560cd8e66bc11b0a14931ce2c2daa48108
ployns <69186797+ployns@users.noreply.github.com>
2022-10-26 15:44:16 +0700

    Add files via upload

---
+++ Creational Design Patterns/creational/CSVBookMetadataExporter.java
@@ -0,0 +1,10 @@
+package creational;
+import java.io.IOException;
+public class CSVBookMetadataExporter extends BookMetadataExporter {
+
+    @Override
+    public BookMetadataFormatter create_book() throws IOException {
+        return new CSVBookMetadataFormatter();
+    }
+    
+}
---
+++ Creational Design Patterns/creational/JSONBookMetadataExporter.java
@@ -0,0 +1,10 @@
+package creational;
+import java.io.IOException;
+
+public class JSONBookMetadataExporter extends BookMetadataExporter{
+    
+    @Override
+    public BookMetadataFormatter create_book() throws IOException{
+        return new JSONBookMetadataFormatter();
+    }
+}
